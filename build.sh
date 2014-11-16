54ba1fe92aeee305035bdfb021d980294eec51c9
Vadim Geshel <vadim@yummly.com>
2014-11-15 20:13:36 -0800

    git submodule

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 im=vgeshel/go-docker-registry
 set -e
 
+git submodule update --init
 # build the executable
 docker build -t $im-stage .
 # build the small image using the executable and other files from the staging image
