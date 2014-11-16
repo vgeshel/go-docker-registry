d5cf6d1c5c49ead80280a82b340ad720600e9f9d
Vadim Geshel <vadim@yummly.com>
2014-11-15 19:53:07 -0800

    build a tiny image

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+im=vgeshel/go-docker-registry
+set -e
+
+# build the executable
+docker build -t $im-stage .
+# build the small image using the executable and other files from the staging image
+bash -evx build-small-image.sh $im $im-stage /usr/src/app/bin/registry
+# stage image no longer needed
+docker rmi $im-stage
